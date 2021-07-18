52deb3a41a1ba685b9be2565ac2e5c891d528f9f
circero <kevyn@axxess.co.za>
2021-07-18 16:46:02 +0200

    2nd

---
+++ build.cmd
@@ -0,0 +1,9 @@
+echo "# repository.weaver" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://www.github.com/CIRCERO/repository.weaver.git
+git push -u origin master
+
+git remote add origin https://www.github.com/CIRCERO/repository.weaver.git
+git push -u origin master
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/kevng/AppData/Roaming/Kodi/addons
+# Build directory: C:/Users/kevng/AppData/Roaming/Kodi/addons/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
